a4b2261f21993059949695b0eafc3784e539fed7
ginokok1996 <ginokok1996@hotmail.com>
2020-08-03 13:49:14 +0200

    subscriber updates

--- api/src/Subscriber/CheckSubscriber.php
+++ api/src/Subscriber/CheckSubscriber.php
@@ -2,26 +2,41 @@
 
 namespace App\Subscriber;
 
+use App\Service\CheckService;
 use ApiPlatform\Core\EventListener\EventPriorities;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Conduction\CommonGroundBundle\Service\CommonGroundService;
 use Symfony\Component\Serializer\SerializerInterface;
 
 
-class WebHookSubscriber implements EventSubscriberInterface
+class CheckSubscriber implements EventSubscriberInterface
 {
     private $params;
-
+    private $checkService;
     private $serializer;
     private $commonGroundService;
 
-    public function __construct(ParameterBagInterface $params, CommongroundService $commonGroundService, SerializerInterface $serializer)
+    public function __construct(ParameterBagInterface $params, CheckService $checkService, CommongroundService $commonGroundService, SerializerInterface $serializer)
     {
+        $this->checkService = $checkService;
         $this->params = $params;
         $this->commonGroundService = $commonGroundService;
         $this->serializer = $serializer;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::VIEW => ['check', EventPriorities::PRE_VALIDATE],
+        ];
+    }
+
+    public function Check(ViewEvent $event)
+    {
+        $check = $event->getControllerResult();
+
     }
 }
