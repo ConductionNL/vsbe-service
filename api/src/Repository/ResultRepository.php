a85a7633c9ae83bcb7b3bcad24d29bda72dd02fd
Gino <ginokok1996@hotmail.com>
2020-08-04 12:01:17 +0200

    Last rename

--- api/src/Repository/ResultRepository.php
+++ api/src/Repository/ResultRepository.php
@@ -16,7 +16,7 @@ class ResultRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
-        parent::__construct($registry, Check::class);
+        parent::__construct($registry, Result::class);
     }
 
     // /**
