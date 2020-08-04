2da35de9b6e6111c3942cd94e297101d9c20d3cd
Gino <ginokok1996@hotmail.com>
2020-08-04 11:53:08 +0200

    rename in ResultRepository

--- api/src/Repository/ResultRepository.php
+++ api/src/Repository/ResultRepository.php
@@ -2,7 +2,7 @@
 
 namespace App\Repository;
 
-use App\Entity\Check;
+use App\Entity\Result;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
