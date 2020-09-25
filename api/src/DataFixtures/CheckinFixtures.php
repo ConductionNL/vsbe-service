d1135d1f6eb0c9ca1a66ae865f8a4ec9b9513d50
Wilco Louwerse <wilco@conduction.nl>
2020-09-25 16:52:44 +0200

    Added new (empty) datafixture files

---
+++ api/src/DataFixtures/CheckinFixtures.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Application;
+use App\Entity\Configuration;
+use App\Entity\Image;
+use App\Entity\Menu;
+use App\Entity\MenuItem;
+use App\Entity\Organization;
+use App\Entity\Slug;
+use App\Entity\Style;
+use App\Entity\Template;
+use App\Entity\TemplateGroup;
+use Conduction\CommonGroundBundle\Service\CommonGroundService;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Ramsey\Uuid\Uuid;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+
+class CheckinFixtures extends Fixture implements DependentFixtureInterface
+{
+    private $params;
+    /**
+     * @var CommonGroundService
+     */
+    private $commonGroundService;
+
+    public function __construct(ParameterBagInterface $params, CommonGroundService $commonGroundService)
+    {
+        $this->params = $params;
+        $this->commonGroundService = $commonGroundService;
+    }
+
+    public function getDependencies()
+    {
+        return [
+            ZuiddrechtFixtures::class,
+        ];
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        if (
+            !$this->params->get('app_build_all_fixtures') &&
+            $this->params->get('app_domain') != 'checking.nu' && strpos($this->params->get('app_domain'), 'checking.nu') == false &&
+            $this->params->get('app_domain') != 'zuid-drecht.nl' && strpos($this->params->get('app_domain'), 'zuid-drecht.nl') == false
+        ) {
+            return false;
+        }
+
+        //do something
+    }
+}
---
+++ api/src/DataFixtures/WestfrieslandFixtures.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Application;
+use App\Entity\Configuration;
+use App\Entity\Image;
+use App\Entity\Menu;
+use App\Entity\MenuItem;
+use App\Entity\Organization;
+use App\Entity\Slug;
+use App\Entity\Style;
+use App\Entity\Template;
+use App\Entity\TemplateGroup;
+use Conduction\CommonGroundBundle\Service\CommonGroundService;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Ramsey\Uuid\Uuid;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+
+class WestfrieslandFixtures extends Fixture
+{
+    private $params;
+    private $commonGroundService;
+
+    public function __construct(ParameterBagInterface $params, CommonGroundService $commonGroundService)
+    {
+        $this->params = $params;
+        $this->commonGroundService = $commonGroundService;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        if (
+            !$this->params->get('app_build_all_fixtures') &&
+            $this->params->get('app_domain') != 'westfriesland.commonground.nu' && strpos($this->params->get('app_domain'), 'westfriesland.commonground.nu') == false
+        ) {
+            return false;
+        }
+
+        //do something
+    }
+}
